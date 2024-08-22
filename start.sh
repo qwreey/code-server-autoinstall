23bcf3a7c38268cb2362da40ca92c1b90b1e797d
Qwreey <me@qwreey.moe>
2024-08-23 04:11:04 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-SPATH="$(realpath "$(dirname "$0")")"
+if [ ! -z "$TARGET" ]; then
+	SPATH="$TARGET"
+else
+	SPATH="$(realpath "$(dirname "$0")")"
+fi
 
 env EXTENSION_GALLERY='{\"serviceUrl\": \"https://marketplace.visualstudio.com/_apis/public/gallery\"}' "$SPATH/code-server/bin/code-server" --auth=none --disable-telemetry --user-data-dir="$SPATH/user-data" --extensions-dir="$SPATH/extensions" --disable-workspace-trust --config "$SPATH/config.yaml" "$@"
 
