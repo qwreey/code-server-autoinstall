5d9f2a3cbe1986d041fe7f344df53488029bcad5
Qwreey <me@qwreey.moe>
2024-08-23 05:15:40 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,6 +7,15 @@ else
 fi
 
 # Version check
+NOW="$(date +%s)"
+TIME_FILE="$SPATH/last-check"
+if [ -e "$TIME_FILE" ]; then
+	if (( $NOW-"$(cat $TIME_FILE)" < 3600 )); then
+		exit 0
+	fi
+fi
+printf "%s" "$NOW" > $TIME_FILE
+
 LATEST="$(curl https://github.com/coder/code-server/releases/latest -i | grep location: | sed -r 's|location: https://github\.com/coder/code\-server/releases/tag/v||' | tr -d '\r')"
 VERSION_FILE="$SPATH/installed-version"
 [ -e "$VERSION_FILE" ] && CURRENT="$(cat "$VERSION_FILE")"
