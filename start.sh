d77e8ace0d96336309a21f282dcd2438a9fc0299
qwreey <me@qwreey.moe>
2024-08-23 23:13:05 +0900

    add patch option

--- start.sh
+++ start.sh
@@ -5,6 +5,14 @@ if [ ! -z "$TARGET" ]; then
 else
 	SPATH="$(realpath "$(dirname "$0")")"
 fi
+if [ -z "$SPATH" ]; then
+	>&2 echo "ERR: SPATH is null ''"
+	exit 1
+fi
+if [ ! -e "$SPATH" ]; then
+	>&2 echo "ERR: SPATH not exist"
+	exit 1
+fi
 
 case ":${PATH}:" in
     *:"$SPATH/bin":*)
@@ -13,5 +21,24 @@ case ":${PATH}:" in
         export PATH="$SPATH/bin:$PATH"
 	;;
 esac
+
+if [ -e "$SPATH/patch" ]; then
+	[ ! -e "$SPATH/code-server/lib/vscode/out/vs/patch" ] && ln -s "$SPATH/patch" "$SPATH/code-server/lib/vscode/out/vs/patch"
+	buf=""
+	for entry in "$SPATH/patch"/*; do
+		name="$(basename -- "$entry")"
+		extension="${name##*.}"
+		if [ extension = "js" ]; then
+			buf+="<script src=\"{{WORKBENCH_WEB_BASE_URL}}/out/vs/patch/$name\"></script>"
+		elif [ extension = "css" ]; then
+			buf+="<link rel="stylesheet" href=\"{{WORKBENCH_WEB_BASE_URL}}/out/vs/patch/$name\">"
+		fi
+	done
+	sed -E "s|^.+</head>\$|    $buf</head>|" -i "$SPATH/code-server/lib/vscode/out/vs/code/browser/workbench/workbench.html"
+else
+	if [ -e "$SPATH/code-server/lib/vscode/out/vs/patch" ]; then
+		rm "$SPATH/code-server/lib/vscode/out/vs/patch"
+	fi
+fi
 
 "$SPATH/code-server/bin/code-server" --user-data-dir="$SPATH/user-data" --extensions-dir="$SPATH/extensions" --config "$SPATH/config.yaml" "$@"
